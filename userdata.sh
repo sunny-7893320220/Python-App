604448301bbcbf3622ee96c8c9222c1cd502b7f8
nisitasubudhi <162950721+nisitasubudhi@users.noreply.github.com>
2024-07-24 16:38:56 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install git
-git clone https://github.com/nisitasubudhi/fish.git
+git clone https://github.com/nisitasubudhi/Fuel-Consumption-Rating.git
 sudo yum -y install python3-pip
 cd Fuel-Consumption-Rating
 pip3 install -r requirements.txt
