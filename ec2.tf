7776c3e07cccf59bd681e79fe345f4f0fdf171d5
sunny-7893320220 <163244207+sunny-7893320220@users.noreply.github.com>
2024-07-24 16:59:11 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "demoinstance" {
-  ami                         = "ami-01fccab91b456acc2"
+  ami                         = "ami-03972092c42e8c0ca"
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "linuxx"
+  key_name                    = "test"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
   subnet_id                   = "${aws_subnet.pub-subnet-1.id}"
   associate_public_ip_address = true   
