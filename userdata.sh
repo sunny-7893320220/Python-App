c2e465cc0be5a607aae86e1e05b82d369338326c
sunny-7893320220 <163244207+sunny-7893320220@users.noreply.github.com>
2024-07-28 13:06:16 +0530

    Update userdata.sh

--- userdata.sh
+++ userdata.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo yum -y update
 sudo yum -y install git
+cd 
 git clone https://github.com/nisitasubudhi/Fuel-Consumption-Rating.git
 sudo yum -y install python3-pip
 cd Fuel-Consumption-Rating
