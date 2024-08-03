c7af17df78a6300ed54cef5d97da119e576622be
sunny-7893320220 <163244207+sunny-7893320220@users.noreply.github.com>
2024-08-03 20:35:56 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,14 +1,14 @@
-resource "aws_instance" "demoinstance" {
+resource "aws_instance" "demo" {
   ami                         = "ami-03972092c42e8c0ca"
   instance_type               = "t2.micro"
   count                       = 1
-  key_name                    = "test"
+  key_name                    = "terrFORM"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
   subnet_id                   = "${aws_subnet.pub-subnet-1.id}"
   associate_public_ip_address = true   
   user_data                   = "${file("userdata.sh")}"
 
   tags = {
-    Name = "demoinstance"
+    Name = "demo"
   }
 }
